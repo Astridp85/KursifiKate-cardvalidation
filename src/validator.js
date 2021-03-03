fa1b4b0e1816f5d5e779fd5477b6897aa9a07d16
Gimena27 <66136801+Gimena27@users.noreply.github.com>
2021-03-03 12:52:58 -0500

    arreglo html, estilos generales css y limpiar validator

--- src/validator.js
+++ src/validator.js
@@ -1,7 +1,1 @@
- // ... Ocultando caracteres y reemplazando por ###
-const validator = 1234567890123456; 
- const lastDig = validator.slice (-4);
-  const masked = lastDig.padStart (16, '#');
-  document.querySelector ('grupoTarjeta').innerHTML = masked;
 
-export default validator;
