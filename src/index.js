80b62cf1bed6be5ee6eed3b3f76223a53a0aebe1
Astridp85 <astrid.pinero85@gmail.com>
2022-03-09 10:54:03 -0500

    Modificación de fotos

--- src/index.js
+++ src/index.js
@@ -1,67 +1,68 @@
-import validator from "./validator.js"
-const formulario = document.getElementById('validacionTarjeta');
+import validator from "./validator.js";
+const formulario = document.getElementById("validacionTarjeta");
 
 // * Select mes dinámico.
-for (let mes = 1; mes <= 12; mes ++ ){
-    let optionMes = document.createElement ("option");
-    optionMes.value = mes;
-    optionMes.innerText = mes;
-    formulario.mesSeleccionado.appendChild(optionMes);
+for (let mes = 1; mes <= 12; mes++) {
+  let optionMes = document.createElement("option");
+  optionMes.value = mes;
+  optionMes.innerText = mes;
+  formulario.mesSeleccionado.appendChild(optionMes);
 }
- // * Select año dinámico.
+// * Select año dinámico.
 const actualYear = new Date().getFullYear();
-for (let year = actualYear; year <= actualYear + 10; year ++){
-    let optionYear = document.createElement ("option");
-    optionYear.value = year;
-    optionYear.innerText = year;
-    formulario.yearSeleccionado.appendChild (optionYear);
+for (let year = actualYear; year <= actualYear + 10; year++) {
+  let optionYear = document.createElement("option");
+  optionYear.value = year;
+  optionYear.innerText = year;
+  formulario.yearSeleccionado.appendChild(optionYear);
 }
 // * número de tarjeta
 formulario.inputNumero1.addEventListener("keyup", (e) => {
-let numberInput = e.target.value;
-//* sin espacios en blanco, letras y eliminando el último espaciado 
-formulario.inputNumero1.value = numberInput
-.replace(/\s/g, '').replace(/\D/g, '')
+  let numberInput = e.target.value;
+  //* sin espacios en blanco, letras y eliminando el último espaciado
+  formulario.inputNumero1.value = numberInput
+    .replace(/\s/g, "")
+    .replace(/\D/g, "")
 
-.trim (); // Elimina el último espacio
+    .trim(); // Elimina el último espacio
 });
 
 // * Validación tarjeta
 //console.log(validator.isValid("12667798654"));
-const saltarInput = document.getElementById ("inputNombre");
+const saltarInput = document.getElementById("inputNombre");
 saltarInput.addEventListener("click", evaluarTarjeta);
-function evaluarTarjeta (){
-var creditCardNumber = document.getElementById ("inputNumero1").value;
-if (validator.isValid(creditCardNumber) == true) {
+function evaluarTarjeta() {
+  var creditCardNumber = document.getElementById("inputNumero1").value;
+  if (validator.isValid(creditCardNumber) == true) {
     //alert ("tarjeta válida");
-    confirm('su tarjeta '+ validator.maskify(creditCardNumber) + ' es válida.'  +  " Desea continuar con su compra");
-}else {
+    confirm(
+      "su tarjeta " +
+        validator.maskify(creditCardNumber) +
+        " es válida." +
+        " Desea continuar con su compra"
+    );
+  } else {
     //alert ("tarjeta no valida");
- confirm('su tarjeta '+ validator.maskify(creditCardNumber) + ' No es válida.'  +  " Desea continuar con su compra");
-
-}
+    confirm(
+      "su tarjeta " +
+        validator.maskify(creditCardNumber) +
+        " No es válida." +
+        " Desea continuar con su compra"
+    );
+  }
 }
 //*Mostrar y ocultar formulario
- const compraOff= document.getElementById("btn4"); 
-compraOff.addEventListener("click",ocultar);
-function ocultar(){
-    document.getElementById("compra").style.display="none";
-    document.getElementById("cursos").style.display="block";
+const compraOff = document.getElementById("btn4");
+compraOff.addEventListener("click", ocultar);
+function ocultar() {
+  document.getElementById("compra").style.display = "none";
+  document.getElementById("cursos").style.display = "block";
 }
-const compraIn= document.getElementById("carrito"); 
+const compraIn = document.getElementById("carrito");
 compraIn.addEventListener("click", mostrar);
-function mostrar(){
-    document.getElementById("compra").style.display="block";
-    document.getElementById("cursos").style.display="none";
+function mostrar() {
+  document.getElementById("compra").style.display = "block";
+  document.getElementById("cursos").style.display = "none";
 }
-/*var compraIn= element.getElementsByClassName('btn');
-compraIn.addEventListener("click", mostrar);
-function mostrar(){
-    document.getElementById("compra").style.display="block";
-    document.getElementById("cursos").style.display="none";
-}*/
-
-
-
 
 
